d6aeaf10feeab4f01764489efa65a7b05ebde011
Victor De la Luz <itztli@gmail.com>
2020-04-23 13:32:03 -0500

    inserting db

--- 02storedb.py
+++ 02storedb.py
@@ -9,8 +9,8 @@ import subprocess
 
 PATH='/home/vdelaluz/public_html/static/'
 
-#with open("db.json", "w") as write_file:
-#    json.dump(config, write_file)
+with open("db.json", "w") as write_file:
+    json.dump(config, write_file)
 
 try:
     cnx = mysql.connector.connect(**config)
